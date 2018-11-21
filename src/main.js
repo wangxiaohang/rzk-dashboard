fb72a12ae5f6c6dffc09e66122a569497fd64036
wxh <xiaohang.wang@aliyun.com>
2018-11-21 18:22:42 +0800

    adding product detail

--- src/main.js
+++ src/main.js
@@ -3,25 +3,23 @@ Vue.use(VueRouter);
 Bmob.initialize("bd871ea12dc290abce3d439aa8cd12aa","5c7a9c2c9b82387a615d8a674e1ebc78");
 
 var temp = {
-  products:[]
+  products:{}
 };
 var sql = {
-  queries: {
-    productsQquery: Bmob.Query("products"),
-  },  
   addingProductFlag: false,
   addProduct: function(info){
+    var query = Bmob.Query("products");
     if(sql.addingProductFlag){
       alert("正在添加，请稍后");
       return;
     };
     sql.addingProductFlag = true;
-    sql.queries.productsQquery.set("name",info.name);
-    sql.queries.productsQquery.set("img",info.img);
-    sql.queries.productsQquery.set("price","￥"+info.price+"/天");
-    sql.queries.productsQquery.set("category",info.category);
-    sql.queries.productsQquery.set("status",0);
-    sql.queries.productsQquery.save().then(res => {
+    query.set("name",info.name);
+    query.set("img",info.img);
+    query.set("price","￥"+info.price+"/天");
+    query.set("category",info.category);
+    query.set("status",0);
+    query.save().then(res => {
       console.log(info.name+"：保存成功");
       sql.addingProductFlag = false;
     }).catch(err => {
@@ -32,10 +30,36 @@ var sql = {
   },
   getProducts: function(fn){
     console.log("开始获取商品数据");
-    sql.queries.productsQquery.find().then(res => {
+    var query = Bmob.Query("products");
+    query.find().then(res => {
       console.log("获取商品数据成功");
+      updateTemp(res);
       fn && fn(res);
     });
+
+    function updateTemp(res){
+      console.log(res);
+      for(var i=0;i<res.length;i++){
+        var pro = res[i];
+        products[pro.id] = {
+          img: pro.img,
+          name: pro.name,
+          price: pro.price,
+          category: pro.category
+        };
+      }
+    }
+  },
+  getProductDetailOf: function(id,fn){
+    var query = Bmob.Query("details");
+    query.equalTo("id","===",id);
+    query.find().then(function(res){
+      console.log(res);
+      fn && fn(res);
+    }).then(function(err){
+      console.log("没有获取到详细信息");
+      fn && fn(null);
+    })
   }
 };
 
@@ -122,6 +146,7 @@ var router = new VueRouter({
             this.newProductDetail.info.push('');
           },
           operatePro: function(row,flag){
+            var that = this;
             switch(flag){
               case 2:
                 // 详情
@@ -131,13 +156,16 @@ var router = new VueRouter({
                 break;
               case 0:
                 // 编辑
-                console.log(row);
+                var price = row.price;
                 this.newProduct.name = row.name;
-                this.newProduct.price = row.price;
+                this.newProduct.price = parseFloat(price.slice(1,price.length-2));
                 this.newProduct.img = row.img;
                 this.newProduct.category = row.category;
                 sql.getProductDetailOf(row.id,function(res){
+                  if(res){
 
+                  };
+                  that.dialogProVisible = true;
                 });
                 break;
             }
